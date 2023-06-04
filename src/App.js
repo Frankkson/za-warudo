0401fae999990d609b5220ae015deca45169231c
Franklin <ffrancadesouza@gmail.com>
2023-06-04 17:02:01 -0300

    implementando sistema inicial de rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Inicio from "./Paginas/Inicio";
+import SobreMim from "./Paginas/SobreMim";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
 	return (
-		<>
-			<h1>Za Warudo (The Wolrd)</h1>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Inicio />}
+				></Route>
+				<Route
+					path="/Sobre-Mim"
+					element={<SobreMim />}
+				></Route>
+				<Route
+					path="*"
+					element={<div>Deu ruim, champs</div>}
+				></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
